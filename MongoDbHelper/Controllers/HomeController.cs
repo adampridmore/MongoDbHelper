ed49b1e957a238fe06a0807f25c8e73089696a26
Adam Pridmore <adam_pridmore@hotmail.com>
2013-02-26 10:14:08 +0000

    - Initial commit

---
+++ MongoDbHelper/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace MongoDbHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index(string objectIds)
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ MongoDbHelper/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MongoDbHelper.MvcApplication" Language="C#" %>
