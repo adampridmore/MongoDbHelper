dcea13139336b2c68513f4397b2e7f1981118332
Adam Pridmore <adam_pridmore@hotmail.com>
2014-03-20 16:42:27 +0000

    - Now will decode a timestamp to an ObjectId value.

---
+++ MongoDbHelper/Controllers/EncodeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MongoDB.Bson;
+using MongoDbHelper.Models;
+
+namespace MongoDbHelper.Controllers
+{
+    public class EncodeController : Controller
+    {
+        //
+        // GET: /Encode/
+
+        public ActionResult Index(string timestamps)
+        {
+            var encodeIndexModel = new EncodeIndexModel();
+
+            if (string.IsNullOrWhiteSpace(timestamps))
+            {
+                return View(encodeIndexModel);
+            }
+
+            var timestampList = timestamps
+                .Split(',')
+                .Where(s => !String.IsNullOrWhiteSpace(s));
+
+            encodeIndexModel.Timestamps = string.Join("\n", timestampList.ToArray());
+
+            var lines = new List<string>();
+            foreach (var timestampText in timestampList)
+            {
+                lines.Add(TimeStampToObjectId(timestampText));
+            }
+
+            encodeIndexModel.ObjectIds = lines;
+            encodeIndexModel.ObjectIdsText = String.Join(Environment.NewLine, lines);
+            
+            return View(encodeIndexModel);
+        }
+
+        private string TimeStampToObjectId(string timestampText)
+        {
+            DateTime dateTime;
+            if (DateTime.TryParse(timestampText, out dateTime))
+            {
+                return new ObjectId(dateTime, 0, 0, 0).ToString();
+            }
+
+            return "Invalid DateTime: " + timestampText;
+        }
+    }
+}
---
+++ MongoDbHelper/Models/EncodeIndexModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MongoDbHelper.Models
+{
+    public class EncodeIndexModel
+    {
+        public string Timestamps { get; set; }
+        public List<string> ObjectIds { get; set; }
+        public string ObjectIdsText { get; set; }
+
+        public EncodeIndexModel()
+        {
+            ObjectIds = new List<string>();
+        }
+    }
+}
